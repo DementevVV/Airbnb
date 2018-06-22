bbc1a1383f225eb06aa9d61bd1ac39d1b7985076
DementevVV <dem_dvv@mail.ru>
2018-06-22 21:43:19 +0300

    controller for photos

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,12 +1,14 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:show]
+  before_action :authorized_user, only: [:edit, :update, :destroy]
 
   def index
     @rooms = current_user.rooms
   end
 
   def show
+    @photos = @room.photos
   end
 
   def new
@@ -17,24 +19,43 @@ class RoomsController < ApplicationController
     @room = current_user.rooms.build(room_params)
 
     if @room.save
-      redirect_to @room, notice: "Saved"
+      if params[:images]
+        params[:images].each do |image|
+          @room.photos.create(image: image)
+        end
+      end
+      
+      @photos = @room.photos
+      redirect_to edit_room_path(@room), notice: "Saved"
     else
       render :new
     end
   end
 
   def edit
+    @photos = @room.photos
   end
 
   def update
     if @room.update(room_params)
-      redirect_to @room, notice: "Updated"
+      if params[:images]
+        params[:images].each do |image|
+          @room.photos.create(image: image)
+        end
+      end
+
+      redirect_to edit_room_path(@room), notice: "Updated"
     else
       render :edit
     end
   end
 
   private
+  def authorized_user
+    @room = current_user.rooms.find_by(params[:id])
+    redirect_to rooms_path, notice: "You don't have permission" if @room.nil?
+  end
+
   def set_room
     @room = Room.find(params[:id])
   end
