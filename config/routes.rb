c872ad356c7073b1ed28469147f048588def99f8
DementevVV <dem_dvv@mail.ru>
2018-06-23 01:48:45 +0300

    ajax for datepicker and price

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
     resources :reservations, only: [:create]
   end
   
+  get '/preload' => 'reservations#preload'
+  get '/preview' => 'reservations#preview'
+
 end
